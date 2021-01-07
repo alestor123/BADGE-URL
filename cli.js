3f5c9bb494416d2bd4d2ef06daa109345eb08ff4
alestor123 <alestoraldous@gmail.com>
2021-01-07 22:15:50 +0530

    added main cli

--- cli.js
+++ cli.js
@@ -1,1 +1,6 @@
 #!/usr/bin/env node
+
+var clipboardy = require('clipboardy'),
+badgeurl = require('./App');
+console.log('Copied !!')
+clipboardy.writeSync(badgeurl(process.argv[2],process.argv[3],process.argv[4]));
